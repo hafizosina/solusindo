4328b4d02421a315560371dc5038c2ae908a7371
hafizosina <hafizosina@gmail.com>
2024-05-29 20:04:35 +0700

    ????

--- src/main/java/com/solusindo/id/config/SecurityConfig.java
+++ src/main/java/com/solusindo/id/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.solusindo.id.config;
 
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+@EnableWebMvc
 public class SecurityConfig {
 
 
@@ -18,23 +24,22 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/actuator/**",
             "/swagger-ui/**",
-            "/login**",
-            "/**"
+            "/login/**",
+            "/error**"
     };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((authorize) -> authorize
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+        ;
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoderss() {
-        return new BCryptPasswordEncoder();
     }
 }
